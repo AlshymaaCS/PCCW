4f1252954aee71ca8426fb48c298c5cccda010ce
Alshymaa <shamscs85@gmail.com>
2018-02-15 11:41:44 +1000

    All tests pass

--- index.js
+++ index.js
@@ -3,24 +3,90 @@
  * See problem 0 in the spec file for the assertion
  * @returns {string}
  */
+
 exports.example = () => {
     return 'hello world';
 };
 
-exports.stripPrivateProperties = () => {};
-exports.excludeByProperty = () => {};
-exports.sumDeep = () => {};
-exports.applyStatusColor = () => {};
-exports.createGreeting = () => {};
-exports.setDefaults = () => {};
+exports.stripPrivateProperties = (properties, items) => {
+    var resultArr = [];
+    items.forEach(item => {
+        var resItem = {};
+        Object.keys(item).forEach(key => {
+            if (properties.indexOf(key) < 0) {
+                resItem[key] = item[key];
+            }
+        });
+        resultArr.push(resItem);
+    });
+    return resultArr;
+};
 
-exports.sanitizeUser = () => {
-    var foundUsersFirstname;
+exports.excludeByProperty = (property, items) => {
+    var resultArr = items.filter(item => !item.hasOwnProperty(property));
+    return resultArr;
+};
 
-    // Create a helper that converts the users name to an array
-    function getNameArray() {
-        return user.name.split('');
+exports.sumDeep = (items) => {
+    var resultArr = [];
+    items.forEach(item => {
+        var resItem = { objects: 0 };
+        item.objects.forEach(o => {
+            resItem.objects += o.val;
+        });
+        resultArr.push(resItem);
+    });
+    return resultArr;
+};
+
+exports.applyStatusColor = (statusCodes, items) => {
+    var statusCodesFlattened = [];
+    Object.keys(statusCodes).forEach(color => {
+        var arr = statusCodes[color].map(code => {
+            var obj = {};
+            obj.code = code;
+            obj.color = color;
+            return obj;
+        });
+        statusCodesFlattened = statusCodesFlattened.concat(arr);
+    });
+
+    var res = [];
+    items.forEach(item => {
+        var colorItem = statusCodesFlattened.find(i => i.code === item.status);
+        if (colorItem) {
+            item.color = colorItem.color;
+            res.push(item);
+        }
+    });
+    return res;
+
+};
+
+exports.createGreeting = (greetFn, message) => {
+    return (name) => {
+        return greetFn(message, name);
+    };
+};
+
+exports.setDefaults = (defaults) => {
+    return (user) => {
+        return Object.assign({}, defaults, user);
     }
+};
+
+exports.sanitizeUser = (user) => {
+
+    // Create a helper that gets first name
+    function getFirstName(fullName) {
+        var firstName = '';
+        if (fullName) {
+            firstName = fullName.split(' ')[0];
+        }
+        return firstName;
+    }
+
+    user.firstName = getFirstName(user.name);
 
     // Ensure a user has an `fullAddress` property by combining `address.streetNum, address.streetName, address.suburb`
     if (user.address.num && user.address.street && user.address.suburb) {
@@ -28,23 +94,7 @@ exports.sanitizeUser = () => {
     }
 
     // The given user always returns the `monthJoined` as 0 to 11. We need it to be 1 to 12 so add 1.
-    user.monthJoined = user.monthJoined + 1;
-
-    // The users name is their full name. We want easy access to the first name.
-    for (i = 0; i < getNameArray().length; i++) {
-
-        // Make sure `firstName` exists and is a String
-        if (!user.firstName) user.firstName = '';
-
-        // We can detect the first name by assuming it is separated with a space. So check if the current character is a space.
-        if (!foundUsersFirstname) foundUsersFirstname = getNameArray()[i] != ' ' ? false : true;
-
-        // If we haven't found the first name yet, append the next character
-        if (getNameArray()[i] && !foundUsersFirstname) {
-            user.firstName = user.firstName + getNameArray()[i];
-        }
-    }
-
+    user.monthJoined +=  1;
     return user;
 };
 
